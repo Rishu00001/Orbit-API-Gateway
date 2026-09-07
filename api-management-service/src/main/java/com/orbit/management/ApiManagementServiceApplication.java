cddcd9dde8f1f531f9c91dd38a2c3bef5859221a
Rishu00001 <ritiksingh7369@gmail.com>
2026-09-07 09:00:26 +0530

    initialize microservices project

---
+++ api-management-service/src/main/java/com/orbit/management/ApiManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.orbit.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiManagementServiceApplication.class, args);
+	}
+
+}
---
+++ smart-gateway-service/src/main/java/com/orbit/gateway/dto/responseDto/HealthResponse.java
@@ -0,0 +1,14 @@
+package com.orbit.gateway.dto.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class HealthResponse {
+    private String serviceName;
+    private String serviceDescription;
+    private String status;
+}
